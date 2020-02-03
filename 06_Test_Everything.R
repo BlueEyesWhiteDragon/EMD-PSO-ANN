a5105513bbab48e9fcf8f45d161c91e98dea7612
jgw <2274462554@qq.com>
2020-02-03 20:05:19 +0800

    ﻿新一轮实验

--- 06_Test_Everything.R
+++ 06_Test_Everything.R
@@ -1,22 +1,102 @@
-### 设置共同参数
-nTest = 30
-nOverlap = 5
-nLastOverlap = 2
-
 ### 纯RBF网络 + 嵌套
-infoRbfAdvanced_1 = getRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nOutput = 1, nHidden = 4, nOverlap = nOverlap, nLastOverlap = nLastOverlap)
+infoRbf_01 = getRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3,  
+  nHidden = 4,
+  nOutput = 1,  
+  nTest = 30, 
+  nOverlap = 5, 
+  nLastOverlap = 3
+)
+
+infoRbf_02 = getRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3,  
+  nHidden = 4,
+  nOutput = 1,  
+  nTest = 30, 
+  nOverlap = 10, 
+  nLastOverlap = 3
+)
+
+infoRbf_03 = getRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3,  
+  nHidden = 4,
+  nOutput = 1,  
+  nTest = 30, 
+  nOverlap = 15, 
+  nLastOverlap = 3
+)
 
 ### PSO-RBF网络 + 嵌套
-infoPsoRbfAdvanced_1 = getPsoRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nHidden = 4, nOutput = 1, nOverlap = nOverlap, nLastOverlap = nLastOverlap, fnscale = 1, maxit = 150, reltol = 0.02, max.restart = 3, trace = 1, trace.stats = T, REPORT = 10)
+infoPsoRbf_01 = getPsoRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3, 
+  nHidden = 4, 
+  nOutput = 1,  
+  nTest = 30, 
+  nOverlap = 5, 
+  nLastOverlap = 3, 
+  fnscale = 1,
+  maxit = 150,
+  reltol = 0.02, 
+  max.restart = 3, 
+  trace = 1,
+  trace.stats = T,
+  REPORT = 10
+)
+
+infoPsoRbf_02 = getPsoRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3, 
+  nHidden = 4, 
+  nOutput = 1,  
+  nTest = 30, 
+  nOverlap = 10, 
+  nLastOverlap = 3, 
+  fnscale = 1,
+  maxit = 150,
+  reltol = 0.02, 
+  max.restart = 3, 
+  trace = 1,
+  trace.stats = T,
+  REPORT = 10
+)
+
+infoPsoRbf_03 = getPsoRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3, 
+  nHidden = 4, 
+  nOutput = 1,  
+  nTest = 30, 
+  nOverlap = 15, 
+  nLastOverlap = 3, 
+  fnscale = 1,
+  maxit = 150,
+  reltol = 0.02, 
+  max.restart = 3, 
+  trace = 1,
+  trace.stats = T,
+  REPORT = 10
+)
 
 ### EMD-PSO-RBF网络 + 嵌套
-infoEmdPsoRbfAdvanced_none_1 = getEmdPsoRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nHidden = 4, nOutput = 1, nOverlap = nOverlap, nLastOverlap = nLastOverlap, boundary = "none", max.imf = 10, fnscale = 1, maxit = 150, reltol = 0.02, max.restart = 3, trace = 1, trace.stats = T, REPORT = 10)
-
-# infoEmdPsoRbfAdvanced_wave_1 = getEmdPsoRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nHidden = 4, nOutput = 1, nOverlap = nOverlap, lastOverlapControl = lastOverlapControl, boundary = "wave", max.imf = 10, fnscale = 1, maxit = 150, reltol = 0.02, max.restart = 3, trace = 1, trace.stats = T, REPORT = 10)
-
-# infoEmdPsoRbfAdvanced_periodic_1 = getEmdPsoRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nHidden = 4, nOutput = 1, nOverlap = nOverlap, lastOverlapControl = lastOverlapControl, boundary = "periodic", max.imf = 10, fnscale = 1, maxit = 150, reltol = 0.02, max.restart = 3, trace = 1, trace.stats = T, REPORT = 10)
-
-# infoEmdPsoRbfAdvanced_evenodd_1 = getEmdPsoRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nHidden = 4, nOutput = 1, nOverlap = nOverlap, lastOverlapControl = lastOverlapControl, boundary = "evenodd", max.imf = 10, fnscale = 1, maxit = 150, reltol = 0.02, max.restart = 3, trace = 1, trace.stats = T, REPORT = 10)
-
-# infoEmdPsoRbfAdvanced_symmetric_1 = getEmdPsoRbfInfoAdvanced(data = maxTemperatureByDay, nTest = nTest, nInput = 3, nHidden = 4, nOutput = 1, nOverlap = nOverlap, lastOverlapControl = lastOverlapControl, boundary = "symmetric", max.imf = 10, fnscale = 1, maxit = 150, reltol = 0.02, max.restart = 3, trace = 1, trace.stats = T, REPORT = 10)
-
+infoEmdPsoRbf_01 = getEmdPsoRbfInfoAdvanced(
+  data = maxTemperatureByDay,
+  nInput = 3, 
+  nHidden = 4, 
+  nOutput = 1, 
+  nTest = 30, 
+  nOverlap = 5, 
+  nLastOverlap = 3, 
+  boundary = "none", 
+  max.imf = 10, 
+  fnscale = 1, 
+  maxit = 150, 
+  reltol = 0.02, 
+  max.restart = 3, 
+  trace = 1, 
+  trace.stats = T, 
+  REPORT = 10
+)
